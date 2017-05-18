4513968336e7336b791fb2d7fcdc96abbeda3320
Pierre ULLRICH <upierre-@e3r11p18.42.fr>
2017-05-18 18:24:55 +0200

    snake

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: upierre- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/11 09:38:08 by upierre-          #+#    #+#             */
-/*   Updated: 2017/05/16 15:06:57 by upierre-         ###   ########.fr       */
+/*   Updated: 2017/05/18 18:24:40 by upierre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,96 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int		ligne(int xi, int yi, int xf, int yf, t_env *e)
+{
+	int		x;
+	int		y;
+	double	a;
+	double	b;
+
+	a = (double) (yf - yi) / (xf - xi);
+	b = yi - a * xi ;
+	x = 200;
+	while(x == xi && x <= xf)
+	{
+		y = (int) (a * x + b);
+		mlx_pixel_put(e->mlx, e->win, x, y, 0x00FF00FF);
+		x++;
+	}
+	x = 0;
+	y = 0;
+/*	while(x <= 2000)
+	{
+		mlx_pixel_put(e->mlx, e->win, x, y, 0x00FF00FF);
+		x++;
+		y++;
+		if ((x % 3) == 0)
+			y--;
+	}*/
+	return(0);
+}
+
 int		my_key_funct(int keycode, t_env *e)
 {
-	printf("key event %d\n", keycode);
-	if (keycode == 12)
-		mlx_pixel_put(e->mlx, e->win, 200, 200, 0x00FF00FF);
-	if (keycode == 13)
-		mlx_pixel_put(e->mlx, e->win, 325, 325, 0x00FF00FF);
+	int		x;
+	int		y;
+	int		xm;
+	int		ym;
+
+	x = 0;
+	y = 0;	
+	if (keycode == 124)
+	{
+		ym = y;
+		while (y <= (ym + 10))
+		{
+			xm = x;
+			while (x <= (xm + 10))
+			{
+				mlx_pixel_put(e->mlx, e->win, x, y, 0x00FF00FF);
+				x++;
+			}
+			y++;
+		}
+	}
+	if (keycode == 123)
+	{
+		ym = y;
+		while (y <= (ym - 10))
+		{
+			xm = x;
+			while (x <= (xm - 10))
+			{
+				mlx_pixel_put(e->mlx, e->win, x, y, 0x00010);
+				x--;
+			}
+			y--;
+		}
+	}
 	return(0);
 }
 
 int		gere_mouse(int button, int x, int y, void *param)
 {
-/*	if (button)
-	{
-		write(1, "\nclic", 5);
-		printf("%d\n", button);
-	}*/
 	printf("button event %d\n", button);
 	(void)*param;
-	(void)x;
-	(void)y;
+	ft_putstr("x = ");
+	ft_putnbr(x);
+	ft_putchar('\n');
+	ft_putstr("y = ");
+	ft_putnbr(y);
+	ft_putchar('\n');
 	return(0);
 }
 
 int		main()
 {
-	int		x;
-	int		y;
 	t_env	e;
 
 	e.mlx = mlx_init();
-	e.win = mlx_new_window(e.mlx, 400, 400, "mlx 42");
-	y = 125;
-	while(y <= 275) 
-	{
-		x = 125;
-		while(x < 275)
-		{
-			mlx_pixel_put(e.mlx, e.win, x, y, 0x00FFFFFF);
-			x++;
-		}
-		y++;
-	}
-	mlx_pixel_put(e.mlx, e.win, 75, 75, 0x00FF00FF);
+	e.win = mlx_new_window(e.mlx, 1500, 1000, "mlx 42");
+	ligne(1, 1, 200, 200, &e);
 	mlx_mouse_hook(e.win, gere_mouse, &e);
 	mlx_key_hook(e.win, my_key_funct, &e);
-//	mlx_mouse_hook(e.win, gere_mouse, &e);
 	mlx_loop(e.mlx);
 }
